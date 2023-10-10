c1d026fb9689834f3f2ad9ab496bbd41cba69336
keshav-star <keshavsandhu00@gmail.com>
2023-10-10 17:57:55 +0530

    pure css

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import heroImage from '../../assets/bg4.png'
+
+const HeroSection = () => {
+    return (
+        <div className="hero-section">
+            <img src={heroImage} className='heroimage' alt="" />
+
+            <div className='detail'>
+                <h1>BEST EDUCATION </h1>
+                <h2>FOR YOUR BETTER FUTURE</h2>
+                <p>We provide always our best service of our clients always </p>
+                <p>Try to client's trust satisfaction</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ src/Pages/Home/RegistrationSection.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const RegistrationSection = () => {
+    const [inputType, setInputType] = useState('text');
+
+    const handleFocus = () => {
+        setInputType('date');
+    };
+
+    const handleBlur = () => {
+        setInputType('text');
+    };
+  return (
+    <div className="registration">
+                <h1>Get A Free <span>Registration</span>! </h1>
+                <form>
+                    <input type="email" name="email" id="email" placeholder='Enter Email' />
+                    <select name="course" id="course">
+                        <option value="Select" disabled selected>Select Course</option>
+                        <option value="course1">course1</option>
+                        <option value="course2">course2</option>
+                    </select>
+                    <input type="number" name="phone" id="phone" placeholder='Phone Number' />
+                    <input name="dob" id="dob"
+                        placeholder='Date Of Birth'
+                        type={inputType}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur} />
+                    <button type="submit" id='submit'>Submit Now</button>
+                </form>
+            </div>
+  )
+}
+
+export default RegistrationSection

4cd3a214ea2f16d74d8dad930268c2effbb1ed89
Pawan Kumar <pyadav.workmail@gmail.com>
2023-10-11 14:15:16 +0530

    Remove student zone folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import videoImg from './assets/video.png'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home';
 import Form from './Pages/form/Form';
-import StudentLogin from './Pages/studentZone/StudentZone';
 import StudentSignup from './Pages/studentZone/studentSignup/StudentSignup';
 import Login from './Pages/studentZone/login/Login';
 

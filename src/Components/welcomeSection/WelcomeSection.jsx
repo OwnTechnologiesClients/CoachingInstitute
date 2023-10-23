2c76469fd3ce9be00f8e9d7253d006dd248cf801
keshav-star <keshavsandhu00@gmail.com>
2023-10-23 21:45:32 +0530

    added notification panel

--- src/Components/welcomeSection/WelcomeSection.jsx
+++ src/Components/welcomeSection/WelcomeSection.jsx
@@ -12,6 +12,15 @@ const WelcomeSection = () => {
     const navigateTo = () => {
         navigate('/about')
     }
+
+    const notifications = [
+        "this is a notification",
+        "this is a notification",
+        "this is a notification",
+        "this is a notification",
+        "this is a notification",
+        "this is a notification",
+    ]
     return (
         <div className="welcome-section">
             <div className="message">
@@ -24,29 +33,24 @@ const WelcomeSection = () => {
             <div className="cards">
                 <WelcomeCard imgSrc={ss2} title="EXPERIENCE YOURSELF" description="Experience Yourself at our coaching center, where personalized guidance, cutting-edge resources, and a supportive learning environment combine to nurture your success. Unleash your full potential with us" />
                 <WelcomeCard imgSrc={ss3} title="ONLINE LEARNING" description="Discover the power of Online Learning at our coaching center. Our interactive, flexible, and engaging courses bring quality education to your fingertips. Embrace a new era of learning and advance your future with us" />
-                <WelcomeCard imgSrc={ss1} title="MASTERY LEARNING" description="At our coaching center, we believe in Mastery Learning. Our tailored programs, expert instructors, and personalized support empower you to master subjects and achieve academic excellence. "/>
-                {/* <div className='notification-panel'>
+                {/* <WelcomeCard imgSrc={ss1} title="MASTERY LEARNING" description="At our coaching center, we believe in Mastery Learning. Our tailored programs, expert instructors, and personalized support empower you to master subjects and achieve academic excellence. "/> */}
+                <div className='notification-panel'>
 
-                    <h2>LATEST UPDATE</h2>
+                    <h2 className='under-bar'>LATEST <span>UPDATES</span></h2>
                     <marquee direction="up" scrollamount="2" onmouseover="this.stop()" onmouseout="this.start()" height="250">
-                        <ul class="list-info--1">
 
-                            <li><strong><a href="#" target="_blank">Admission Notice for left over seats for B.Tech. 1st year and B.Tech. 2nd Year (LEET) 13/10/2023</a></strong></li>
+                        {
+                            notifications.map((item,index)=>{
+                                return <ul key={index} className="list-info--1">
 
-                        </ul>
-                        <ul class="list-info--1">
-
-                            <li><strong><a href="#" target="_blank">Admission Notice for left over seats for B.Tech. 1st year and B.Tech. 2nd Year (LEET) 13/10/2023</a></strong></li>
-
-                        </ul>
-                        <ul class="list-info--1">
-
-                            <li><strong><a href="#" target="_blank">Admission Notice for left over seats for B.Tech. 1st year and B.Tech. 2nd Year (LEET) 13/10/2023</a></strong></li>
-
-                        </ul>
-
+                                <li><a href="#" target="_blank">{item}</a></li>
+    
+                            </ul>
+                            })
+                        }
+                        
                     </marquee>
-                </div> */}
+                </div>
 
             </div>
         </div>

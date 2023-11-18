1dfb2288e1247183fd4ab9877d7c7c224782c9e7
keshav-star <keshavsandhu00@gmail.com>
2023-11-18 12:44:26 +0530

    added playlist in videos page

--- src/Pages/popularVideos/PopularVideos.jsx
+++ src/Pages/popularVideos/PopularVideos.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../Components/navbar/Navbar'
 import { Header1, Header2 } from '../../components/header/Header'
 import HeroSection from '../../components/heroSection/HeroSection'
@@ -10,17 +10,55 @@ import Footer from '../../components/footer/Footer'
 import WhatsappIcon from '../../components/whatsappIcon/WhatsappIcon'
 
 const PopularVideos = () => {
+
+    const [activePlaylist, setactivePlaylist] = useState('Quantum Chemistry');
+    const [activePlaylistLink, setactivePlaylistLink] = useState('PL2EYCqZ7oXvLKiyVMg6AaWFNYVkmmOEZj');
+
     let videoLinks = [
         "https://www.youtube.com/embed/4jjc6yb7af8?si=DYNPueT5B7zjva8Z",
         "https://www.youtube.com/embed/vi7mVuQ8-BA?si=cuPpJmw_5UMeQJ3R",
         "https://www.youtube.com/embed/4jjc6yb7af8?si=DYNPueT5B7zjva8Z",
-        "https://www.youtube.com/embed/vi7mVuQ8-BA?si=cuPpJmw_5UMeQJ3R",
-        "https://www.youtube.com/embed/4jjc6yb7af8?si=DYNPueT5B7zjva8Z",
-        "https://www.youtube.com/embed/vi7mVuQ8-BA?si=cuPpJmw_5UMeQJ3R",
-        "https://www.youtube.com/embed/4jjc6yb7af8?si=DYNPueT5B7zjva8Z",
-        "https://www.youtube.com/embed/vi7mVuQ8-BA?si=cuPpJmw_5UMeQJ3R",
-        "https://www.youtube.com/embed/4jjc6yb7af8?si=DYNPueT5B7zjva8Z"
+
     ]
+
+    let playlists = [
+        {
+            name: "Chemical Science",
+            image: "playlist/p1.png",
+            numberofvideos: 5,
+            playlistLink: "https://www.youtube.com/playlist?list=PL2EYCqZ7oXvLKiyVMg6AaWFNYVkmmOEZj"
+        },
+        {
+            name: "Atomic Science",
+            image: "playlist/p2.png",
+            numberofvideos: 25,
+            playlistLink: "https://www.youtube.com/playlist?list=PL2EYCqZ7oXvLlQZbB8xVJh0mwomAiJeGF"
+        },
+        {
+            name: "Physical Science",
+            image: "playlist/p3.png",
+            numberofvideos: 15,
+            playlistLink: "https://www.youtube.com/playlist?list=PL2EYCqZ7oXvLKiyVMg6AaWFNYVkmmOEZj"
+        },
+    ]
+
+    const handleActivePlaylist = (item) => {
+
+        const pllink = item.playlistLink
+
+        const regex = /[?&]list=([^&]+)/;
+        const match = pllink.match(regex);
+
+        if (match) {
+            const playlistId = match[1];
+            setactivePlaylistLink(playlistId);
+        } else {
+            console.error('Invalid YouTube playlist link');
+        }
+
+        setactivePlaylist(item.name);
+    }
+
     return (
         <div className='study-material-page'>
 
@@ -37,11 +75,31 @@ const PopularVideos = () => {
             <div className="popular-videos-section">
                 <h2 className='under-bar'><span>POPULAR</span> VIDEOS</h2>
                 <div className="video-cards">
-                {videoLinks.map((link,index)=>{
-                   return <VideoCard key={index} videourl={link}/>
-                })}
+                    {videoLinks.map((link, index) => {
+                        return <VideoCard key={index} videourl={link} />
+                    })}
+                </div>
+
+                <h2 className='under-bar'><span>ALL PLAYLIST</span> VIDEOS</h2>
+                <div className='playlist-cards'>
+                    {
+                        playlists.map((item, index) => {
+                            return <div key={index} className="card"
+                                onClick={() => handleActivePlaylist(item)}>
+                                <h1>{item.numberofvideos}+ Videos</h1>
+                                <iframe width='370' height="220" src={`https://www.youtube.com/embed/videoseries?si=Z02FiG3_6G0HTb2M&amp;list=${activePlaylistLink}`} title="YouTube video player" frameborder="0" ></iframe>
+                                <h3>{item.name}</h3>
+                            </div>
+                        })
+                    }
+                </div>
+
+                <div className="main-video-section">
+                    <h2 className='under-bar'>{activePlaylist}</h2>
+                    <iframe height="690" src={`https://www.youtube.com/embed/videoseries?si=Z02FiG3_6G0HTb2M&amp;list=${activePlaylistLink}`} title="YouTube video player" frameborder="0" ></iframe>
                 </div>
             </div>
+
             <WhatsappIcon />
             <Footer />
 

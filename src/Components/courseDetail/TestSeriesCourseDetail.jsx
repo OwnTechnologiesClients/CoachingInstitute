07ec4bf90bd442a74f91d868a195482228c800df
keshav-star <keshavsandhu00@gmail.com>
2023-12-01 16:13:26 +0530

    test series content added

---
+++ src/Components/courseDetail/TestSeriesCourseDetail.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './CourseDetail.scss'
+const TestSeriesCourseDetail = ({ imgSrc, heading1, detailData }) => {
+    return (
+        <div className='course-detail'>
+            <div className="course-img">
+                <img src={detailData[heading1].image} alt="" />
+            </div>
+            <div className="content">
+                <h2>{heading1}</h2>
+
+                <p>{detailData[heading1]?.coursedHeading}</p>
+                <p style={{color:"#000"}}>Enroll Today and Embrace Success in {heading1.replace('(','').replace(')','')}!</p>
+
+                <div className="why-join-container">
+                    <h2>Why Opt for ASAP Institute's {heading1.replace('(','').replace(')','')} Test Series?</h2>
+                    {Object.keys(detailData[heading1].courseLines).map((item, index) => {
+                        return <div key={index} className='why-join'>
+                            <p><strong>{item}</strong>:  {detailData[heading1].courseLines[item]}</p>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TestSeriesCourseDetail
